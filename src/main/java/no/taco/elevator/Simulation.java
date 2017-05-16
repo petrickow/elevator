a6d18137bc0621ea834c5ac2665211e7f9a05377
Cato Danielsen <catodanielsen@gmail.com>
2017-05-16 23:15:54 +0200

    notes on structure

--- src/main/java/no/taco/elevator/Simulation.java
+++ src/main/java/no/taco/elevator/Simulation.java
@@ -10,14 +10,18 @@ public class Simulation {
         /* TODO:
          * Setup --> antall heiser, besøkende, ticks -->
          * Modeller:
-          * Tick
-          * Elevator
-          * Building
-          * Agent
+          * Tick <- klokke, holder styr på tidsforløp i simulering
+          * Elevator <- modell for en gitt heis, kapasitet, på vei opp/ned, antall passasjerer
+          * Agent <- besøkende, states: fremme, kø, i heis
+          * Building <- container for heiser og agenter og antall etasjer
+
+            Hvordan skal heiser reagere på request? Modellere opp/ned request?
          */
     }
 
-    public int initSimulation(int numElevators, int ) {
+    public int initSimulation(int numElevators, int visitors) {
 
+
+        return 0;
     }
 }
